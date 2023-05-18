22230f0b99d0dfac2f8c0cf09f9b8dde81072cd6
jonsalkin <50082962+jonsalkin@users.noreply.github.com>
2023-05-18 11:18:33 -0400

    Initial Commit

---
+++ video player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  video player
+//
+//  Created by Jon Salkin on 5/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
